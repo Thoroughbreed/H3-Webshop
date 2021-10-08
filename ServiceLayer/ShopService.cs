45dcb76aa28a66a1a23d6df87e5792f1293042f7
jan <jan@W10BOOTCAMP>
2021-10-08 09:36:59 +0200

    Added summary to service layer

--- ServiceLayer/ShopService.cs
+++ ServiceLayer/ShopService.cs
@@ -15,6 +15,16 @@ namespace ServiceLayer
             _context = ct;
         }
 
+        /// <summary>
+        /// Adding a new user to context
+        /// </summary>
+        /// <param name="FName">First name</param>
+        /// <param name="LName">Last name</param>
+        /// <param name="RoadName">Road name</param>
+        /// <param name="RoadNum">Road number</param>
+        /// <param name="PostNum">Post number</param>
+        /// <param name="PhoneMain">Phone number (main)</param>
+        /// <param name="PhoneCell">Phone number (secondary)</param>
         public void AddUser(string FName, string LName, string RoadName, int RoadNum, int PostNum, string? PhoneMain, string? PhoneCell)
         {
             Customers cust = new Customers
@@ -31,6 +41,10 @@ namespace ServiceLayer
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Deleting user from context
+        /// </summary>
+        /// <param name="customer">Customer object</param>
         public void DeleteUser(Customers customer)
         {
             var user = _context.Customers.Where(u => u == customer).FirstOrDefault();
@@ -38,6 +52,17 @@ namespace ServiceLayer
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Editing already existing user
+        /// </summary>
+        /// <param name="userID">Customer ID</param>
+        /// <param name="FName">First name</param>
+        /// <param name="LName">Last name</param>
+        /// <param name="RoadName">Road name</param>
+        /// <param name="RoadNum">Road number</param>
+        /// <param name="PostNum">Post number</param>
+        /// <param name="PhoneMain">Phone number (main)</param>
+        /// <param name="PhoneCell">Phone number (secondary)</param>
         public void EditUser(int userID, string FName, string LName, string RoadName, int RoadNum, int PostNum, string? PhoneMain, string? PhoneCell)
         {
             Customers cust = _context.Customers.Where(u => u.CustomerID == userID).FirstOrDefault();
@@ -52,6 +77,10 @@ namespace ServiceLayer
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Creating a new order connected to current user
+        /// </summary>
+        /// <param name="customer">Customer object</param>
         public void AddOrder(Customers customer)
         {
             Orders order = new Orders
