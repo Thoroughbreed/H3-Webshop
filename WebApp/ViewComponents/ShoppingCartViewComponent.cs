2179aec36b179d177f9e0e7b8ed64ad010d2b79f
Jan Andreasen <JAA@hartmann-packaging.com>
2021-10-19 20:46:01 +0200

    Added shopping basket VC

---
+++ WebApp/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer;
+
+namespace WebApp.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly ShopService _service;
+        public ShoppingCartViewComponent(ShopService service)
+        {
+            _service = service;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            int counter = 3;
+            return View(counter);
+        }
+    }
+}
