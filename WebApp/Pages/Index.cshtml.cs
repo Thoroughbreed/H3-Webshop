c379e8280e83f3769100695a2147c55635a6417c
Jan Andreasen <JAA@hartmann-packaging.com>
2021-10-21 08:53:04 +0200

    Wait a minute...

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        [BindProperty(SupportsGet = true)]
-        public string TestString { get; set; } = "YES!";
 
+        
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
